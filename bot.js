716bbef753c9539a7e87294b2a87d57f72fdb417
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-05-13 02:52:43 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const {Client, RichEmbed, Message, DiscordAPIError} = require('discord.js') ;
 const Discord = require('discord.js');
 const client = new Client ;
-const setting = require('./settings.json') ;
 
 let CLO_embed = new Discord.MessageEmbed()
 .setTitle("[點選Emoji就能修改下方的文字視窗哦]")
