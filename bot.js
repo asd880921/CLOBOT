56b75da7f5fb25bd1d3290303f0b663aba8f4971
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-05-13 18:25:34 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,8 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (message.content === '!查詢') {
         message.channel.send(CLO_embed)
+	message.edit().then(msg => msg.edit(CLO_embed.setDescription("```[翻譯列表-道具名稱會顯示在這邊哦]```")
+        ,CLO_embed.setThumbnail("https://i.imgur.com/eJNvuBH.png"),CLO_embed.setTimestamp()))
     }
     if(message.embeds = CLO_embed && message.author.bot){
         for (var i = 0; i < length; i++) {
