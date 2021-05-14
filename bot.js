1baa58c71cd37fbe378a12f66c2ef38407ad319b
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-05-14 16:49:52 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,10 +65,7 @@ client.on('messageReactionAdd', (messageReaction, user) => {
 
     const { message, emoji} = messageReaction;
 	
-	
-    _count++
-	
-	
+		
     message.edit().then(msg=>{
         msg.edit(CLO_embed.setDescription("```[加載中Loading....]```").setThumbnail("https://i.imgur.com/eJNvuBH.png").setTimestamp())
     })
@@ -76,77 +73,92 @@ client.on('messageReactionAdd', (messageReaction, user) => {
 	
     switch(emoji.id){
         case '841661109868953651':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[영롱한 주얼 컴포넌트]```").setThumbnail("https://cdn.discordapp.com/emojis/841661109868953651.png"))
             })
             break;
         case '841661110440034334':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[초월의 비약]```").setThumbnail("https://cdn.discordapp.com/emojis/841661110440034334.png"))
             })
             break;
         case '841662779436957697':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[일반 D 컴포넌트 : 기어]```").setThumbnail("https://cdn.discordapp.com/emojis/841662779436957697.png"))
             })
             break;
         case '841663111878017084':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[일반 D 컴포넌트 : 코스튬]```").setThumbnail("https://cdn.discordapp.com/emojis/841663111878017084.png"))
             })
             break;
         case '841663779212230657':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[품질 보증 씰]```").setThumbnail("https://cdn.discordapp.com/emojis/841663779212230657.png"))
             })
             break;
         case '841662235410432011':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[슬롯 타입 변경툴]```").setThumbnail("https://cdn.discordapp.com/emojis/841662235410432011.png"))
             })
             break;
         case '841663477433106442':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[리얼 플래티넘 드라이버]```").setThumbnail("https://cdn.discordapp.com/emojis/841663477433106442.png"))
             })
             break;
         case '841662530849472542':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[일반 강화기 연료]```").setThumbnail("https://cdn.discordapp.com/emojis/841662530849472542.png"))
             })
             break;
         case '841662438449741897':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[유니온 기어 윤활제]```").setThumbnail("https://cdn.discordapp.com/emojis/841662438449741897.png"))
             })
             break;
         case '842632313620004885':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[영롱한 주얼 이퀄라이저]```").setThumbnail("https://cdn.discordapp.com/emojis/842632313620004885.png"))
             })
             break;
         case '841663979914526761':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[유니온 네임택]```").setThumbnail("https://cdn.discordapp.com/emojis/841663979914526761.png"))
             })
             break;
 	case '842371663994814465':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[솔로몬 싱크로 섬유]```").setThumbnail("https://cdn.discordapp.com/emojis/842371663994814465.png"))
             })
             break;
             break;
 	case '842371760858071050':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[샤이닝 싱크로 섬유]```").setThumbnail("https://cdn.discordapp.com/emojis/842371760858071050.png"))
             })
             break;
 	case '842633204058161162':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[코스튬 퓨전 섬유]```").setThumbnail("https://cdn.discordapp.com/emojis/842633204058161162.png"))
             })
             break;
 	case '842371471161819156':
+		    _count++
             message.edit().then(msg=>{
                 msg.edit(CLO_embed.setDescription("```[스킬 D 컴포넌트  기어]```").setThumbnail("https://cdn.discordapp.com/emojis/842371471161819156.png"))
             })
