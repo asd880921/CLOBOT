e06f12705758434ec79a6dd2c2a403ff1913689b
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-05-14 23:39:13 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,9 +166,8 @@ client.on('messageReactionAdd', (messageReaction, user) => {
             break;
 		default:
 		    _count++
-		    var _name=user.reactions.author.id;
             message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```<@"+_name+">你按這個是要衝3小```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
+                msg.edit(CLO_embed.setDescription("```<@"+user.id+">你按這個是要衝3小```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
             })
             break;
     }
