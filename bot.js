731e01663ef26e36a94a6ab5f148201c73772bba
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-05-14 14:09:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,11 +66,12 @@ client.on('messageReactionAdd', (messageReaction, user) => {
     const { message, emoji} = messageReaction;
     _count++
 	
+    message.reactions(emoji.id).users.remove(user.id).catch(err => {})
+	
     message.edit().then(msg=>{
         msg.edit(CLO_embed.setDescription("```[加載中Loading....]```").setThumbnail("https://i.imgur.com/eJNvuBH.png").setTimestamp())
     })
 
-    message.reactions.resolve(emoji.id).users.remove(user.id).catch(err => {})
 	
     switch(emoji.id){
         case '841661109868953651':
