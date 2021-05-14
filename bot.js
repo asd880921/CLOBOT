95174cfe6ce1e2988392dd67f18a0dee02a0ba4b
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-05-14 16:21:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.on('messageReactionAdd', (messageReaction, user) => {
 
     const { message, emoji} = messageReaction;
 	
-messageReaction.message.reactions.removeAll();
+message.reactions.removeAll();
 	
     _count++
 	
