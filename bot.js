49d276d30f7dc58fb4124647a132c4a0547e7816
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-05-15 00:42:31 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,15 @@ const {Client, RichEmbed} = require('discord.js') ;
 const Discord = require('discord.js');
 const client = new Client ;
 const {token} = require('./settings.json') ;
-var SetCount = 28;
+var SetCount = 30;
 var _count = SetCount
 var BOT_Embed = false;
+
+//字典
+var Item = require('./Item.json');
+var CLOEmoji_ID =Object.keys(Item);
+var length = CLOEmoji_ID.length;
+
 
 let CLO_embed = new Discord.MessageEmbed()
 .setTitle("[點選Emoji就能拿到道具名稱了哦]")
@@ -16,19 +22,6 @@ let CLO_embed = new Discord.MessageEmbed()
 .setThumbnail("https://i.imgur.com/eJNvuBH.png")
 .setAuthor("封印者道具翻譯","https://i.imgur.com/5yOdbez.png","https://closers.nexon.com/Main/Index")
 
-
-var CLO_Emoji=[
-'841661109868953651','841661110440034334',
-'841662235410432011','841662779436957697',
-'841663111878017084','841663779212230657',
-'841662530849472542','841663477433106442',
-'841662438449741897','841663979914526761',
-'842371663994814465','842371760858071050',
-'842633204058161162','842632313620004885',
-'842371471161819156','842757425739333683'
-];
-
-var length = CLO_Emoji.length;
     
 client.on('ready', () => {
     console.log(`${client.user.tag} 準備好上戰場惹！～`);
@@ -44,7 +37,7 @@ client.on('message', message => {
     if(BOT_Embed && message.author.bot){
 	BOT_Embed = false;
         for (var i = 0; i < length; i++) {
-        message.react(CLO_Emoji[i]);
+        message.react(CLOEmoji_ID[i]);
         }
     }
 	if(message.content === '!次數') {
@@ -65,112 +58,27 @@ client.on('messageReactionAdd', (messageReaction, user) => {
     if(user.bot)  return;
 
     const { message, emoji} = messageReaction;
-	
-		
+
+
     message.edit().then(msg=>{
         msg.edit(CLO_embed.setDescription("```[加載中Loading....]```").setThumbnail("https://i.imgur.com/eJNvuBH.png").setTimestamp())
     })
 
-	
-    switch(emoji.id){
-        case '841661109868953651':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[영롱한 주얼 컴포넌트]```").setThumbnail("https://cdn.discordapp.com/emojis/841661109868953651.png"))
-            })
-            break;
-        case '841661110440034334':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[초월의 비약]```").setThumbnail("https://cdn.discordapp.com/emojis/841661110440034334.png"))
-            })
-            break;
-        case '841662779436957697':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[일반 D 컴포넌트 : 기어]```").setThumbnail("https://cdn.discordapp.com/emojis/841662779436957697.png"))
-            })
-            break;
-        case '841663111878017084':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[일반 D 컴포넌트 : 코스튬]```").setThumbnail("https://cdn.discordapp.com/emojis/841663111878017084.png"))
-            })
-            break;
-        case '841663779212230657':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[품질 보증 씰]```").setThumbnail("https://cdn.discordapp.com/emojis/841663779212230657.png"))
-            })
-            break;
-        case '841662235410432011':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[슬롯 타입 변경툴]```").setThumbnail("https://cdn.discordapp.com/emojis/841662235410432011.png"))
-            })
-            break;
-        case '841663477433106442':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[리얼 플래티넘 드라이버]```").setThumbnail("https://cdn.discordapp.com/emojis/841663477433106442.png"))
-            })
-            break;
-        case '841662530849472542':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[일반 강화기 연료]```").setThumbnail("https://cdn.discordapp.com/emojis/841662530849472542.png"))
-            })
-            break;
-        case '841662438449741897':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[유니온 기어 윤활제]```").setThumbnail("https://cdn.discordapp.com/emojis/841662438449741897.png"))
-            })
-            break;
-        case '842632313620004885':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[영롱한 주얼 이퀄라이저]```").setThumbnail("https://cdn.discordapp.com/emojis/842632313620004885.png"))
-            })
-            break;
-        case '841663979914526761':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[유니온 네임택]```").setThumbnail("https://cdn.discordapp.com/emojis/841663979914526761.png"))
-            })
-            break;
-	case '842371663994814465':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[솔로몬 싱크로 섬유]```").setThumbnail("https://cdn.discordapp.com/emojis/842371663994814465.png"))
-            })
-            break;
-            break;
-	case '842371760858071050':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[샤이닝 싱크로 섬유]```").setThumbnail("https://cdn.discordapp.com/emojis/842371760858071050.png"))
-            })
-            break;
-	case '842633204058161162':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[코스튬 퓨전 섬유]```").setThumbnail("https://cdn.discordapp.com/emojis/842633204058161162.png"))
-            })
-            break;
-	case '842371471161819156':
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("```[스킬 D 컴포넌트  기어]```").setThumbnail("https://cdn.discordapp.com/emojis/842371471161819156.png"))
-            })
-            break;
-		default:
-		    _count++
-            message.edit().then(msg=>{
-                msg.edit(CLO_embed.setDescription("<@"+user.id+">"+"```你按這個是要衝3小```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
-            })
-            break;
+
+    if(Item.hasOwnProperty(emoji.id) && !(emoji.id == "842757425739333683")){
+        _count++
+        message.edit().then(msg=>{
+            msg.edit(CLO_embed.setDescription("```"+Item[emoji.id]+"```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
+        })
     }
+    else
+    {
+        _count++
+        message.edit().then(msg=>{
+            msg.edit(CLO_embed.setDescription("<@"+user.id+">"+"```你按這個是要衝3小```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
+        })
+    }
+
 });
 
 client.login(process.env.BOT_TOKEN) ;
