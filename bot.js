999d2d5f01bb6aeaa0cb034b445d0e519ae04e6f
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-06-11 14:30:19 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,26 +63,26 @@ client.on('message',msg=>{
 }) ;
 
 
-client.on('messageReactionAdd',async (messageReaction, user) => {
+client.on('messageReactionAdd',(messageReaction, user) => {
     if(user.bot)  return;
     const { message, emoji} = messageReaction;
     if(!(message_id == message.id)) return;
 
 
-    await message.edit().then(msg=>{
-       msg.edit(CLO_embed.setDescription("```[加載中Loading....]```").setThumbnail("https://i.imgur.com/eJNvuBH.png").setTimestamp())
-    })
+    //message.edit().then(msg=>{
+       //msg.edit(CLO_embed.setDescription("```[加載中Loading....]```").setThumbnail("https://i.imgur.com/eJNvuBH.png").setTimestamp())
+   // })
 
-    if(Item.hasOwnProperty(emoji.id) && !(emoji.id == "842757425739333683")){
-        await message.edit().then(msg=>{
-           msg.edit(CLO_embed.setDescription("```"+Item[emoji.id]+"```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
-        })
-    }
-    else
-    {
-        await message.edit().then(msg=>{
-           msg.edit(CLO_embed.setDescription("<@"+user.id+">"+"```你按這個是要衝3小```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
-        })
+    //if(Item.hasOwnProperty(emoji.id) && !(emoji.id == "842757425739333683")){
+        //message.edit().then(msg=>{
+           //msg.edit(CLO_embed.setDescription("```"+Item[emoji.id]+"```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
+        //})
+    //}
+    //else
+    //{
+        //message.edit().then(msg=>{
+           //msg.edit(CLO_embed.setDescription("<@"+user.id+">"+"```你按這個是要衝3小```").setThumbnail("https://cdn.discordapp.com/emojis/"+emoji.id+".png"))
+        //})
     }
 });
 
