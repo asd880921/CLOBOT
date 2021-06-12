c3c3f75643b283304e6d1ddce128a3d38f9bb78d
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-06-12 21:54:20 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,6 @@ client.on('message',msg=>{
 	if(msg.content.includes('星爆')){
 	msg.react('802488806192185373');
 	}
-}
 
 client.on('messageReactionAdd',(messageReaction, user) => {
     if(user.bot)  return;
