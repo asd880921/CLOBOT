c9a138608d3b317f6c75a4d0b258480bcf4e2c69
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-08-13 18:09:35 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,7 +73,7 @@ client.on('message',msg=>{
 		var str = msg.content;
 		key_word.key.push(str.substring(5,100));
 		var result = JSON.stringify(key_word);
-		key_word.writeFile("./Item.json",result,"utf8")
+		fs.writeFile("./Item.json",result,"utf8")
 	}
 });
 
