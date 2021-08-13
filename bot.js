19d61386f05b7eeec7292ea295fbaf9a2addc97e
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-08-13 15:51:18 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ client.on('message',msg=>{
 
 client.on('message',msg=>{
 	for (var i in key_word){
-		if(msg.content.includes(key_word[i])){
+		if(msg.content.includes(key_word.key[i])){
 		   	for (var i = 0; i < Me_length; i++) {
 			msg.react(Meme_Emoji[i]);
 		}
