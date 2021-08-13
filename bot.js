57b47a06484e4584bfcb3a91658a02643721c4e1
asd880921 <76730567+asd880921@users.noreply.github.com>
2021-08-13 17:54:31 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ client.on('message',msg=>{
 //把關鍵字寫入key_word.JSON
 var strmsg = "";
 client.on('message',msg=>{
-	if(msg.content.includes('!add(key)@'){
+	if(msg.content.includes('!add(key)@')){
 	strmsg = strkey_word.key.push(msg.substring(10, 100));
 	var result = JSON.stringify(strkey_word);
 }
